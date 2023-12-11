cac7795acf0a86aac79419e67208f41642485379
Retanaar <khorovinkin@gmail.com>
2023-12-11 13:46:34 +0200

    Send Query for last messages

--- client/telegram/telegram.go
+++ client/telegram/telegram.go
@@ -1,6 +1,12 @@
 package telegram
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"path"
+	"strconv"
+)
 
 type Client struct {
 	host     string
@@ -16,8 +22,32 @@ func New(host, token string) Client {
 	}
 }
 
-func (c *Client) Updates() ([]Update, error) {
+func (c *Client) Updates(offset, limit int) ([]Update, error) {
+	q := url.Values{}
+	q.Add("offset", strconv.Itoa(offset))
+	q.Add("limit", strconv.Itoa(limit))
+}
 
+func (c *Client) doRequest(method string, query url.Values) ([]byte, error) {
+	u := url.URL{
+		Scheme: "https",
+		Host: c.host,
+		Path: path.Join(c.basePath, method),
+	}
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("can`t do request: %w", err)
+	}
+
+	req.URL.RawQuery = query.Encode()
+
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("can`t do request: %w", err)
+	}
+
+	return resp, nil
 }
 
 func (c *Client) sendMessage() {
